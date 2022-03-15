ef1bd7d3355113381bdbeb0608f9ee450e27a86a
trungta-at-168084223882 <trung.ta@wolfconsulting.com.sg>
2022-03-15 12:00:46 +0700

    faile

--- app/Models/Bnpl.php
+++ app/Models/Bnpl.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Jenssegers\Mongodb\Eloquent\Model;
 use Maklad\Permission\Traits\HasRoles;
 class Bnpl extends Model
