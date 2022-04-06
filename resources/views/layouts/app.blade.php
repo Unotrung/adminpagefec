cfd73c3a850d1e1c9bb839f316522e661a8e03f6
trungta-at-168084223882 <trung.ta@wolfconsulting.com.sg>
2022-04-06 09:48:35 +0700

    update ver1.0.3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 @extends('adminlte::page')
 @section('footer')
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0.2 05-04-2022
+      <b>Version</b> 1.0.3 06-04-2022
     </div>
     <strong>Copyright &copy; 2022 <a href="#">Voolo Admin</a>.</strong> All rights reserved.
 @stop
