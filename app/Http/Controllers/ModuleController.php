86345a057b245eef0e4c8c0759683dcc90f2322c
trungta-at-168084223882 <trung.ta@wolfconsulting.com.sg>
2022-04-14 16:35:04 +0700

    update version

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -114,6 +114,7 @@ class ModuleController extends Controller
         */
         $role->permission_ids = [];
         $role->save();
+
         $permissions = $request->permissions;
         foreach ($permissions as $ele){
             $is_exist = Permission::firstOrCreate(['name' => $ele]);
@@ -146,22 +147,11 @@ class ModuleController extends Controller
         if ($request->ajax()) {
            $out =  Datatables::of(Modules::whereNull("is_delete")->get())->make(true);
            $data = $out->getData();
+           $role = Role::all();
            for($i=0; $i < count($data->data); $i++) {
                $output = '';
-               //$output .= ' <a href="'.url(route('modules.show',['id'=>$data->data[$i]->_id])).'" class="btn btn-info btn-xs" style="display:inline;padding:2px 5px 3px 5px;"><i class="fa fa-eye"></i></a>';
-                //$output .= ' <a href="'.url(route('modules.edit',['id'=>$data->data[$i]->_id])).'" class="btn btn-warning btn-xs" style="display:inline;padding:2px 5px 3px 5px;"><i class="fa fa-edit"></i></a>';
-               //$output .= ' <a href="'.url(route('modules.delete',['id'=>$data->data[$i]->_id])).'" class="btn btn-danger btn-xs" style="display:inline;padding:2px 5px 3px 5px;"><i class="fa fa-ban"></i></a>';
                $data->data[$i]->action = (string)$output;
-           //     if($this->show_action) {
-           //         $output = '';
-           // //         // $output .= '<button class="btn btn-warning btn-xs" label="Open Modal" data-toggle="modal" data-target="#exampleModal" type="submit"><i class="fa fa-edit"></i></button>';
-           //         $output .= ' <a href="'.url(route('bnpl.edit').'/'.$data->data[$i]->_id).'" class="btn btn-warning btn-xs" style="display:inline;padding:2px 5px 3px 5px;"><i class="fa fa-edit"></i></a>';
-           // //         // $output .= ' <a href="'.url(route('employee.show',['id'=>$data->data[$i]->_id])).'" class="btn btn-info btn-xs" style="display:inline;padding:2px 5px 3px 5px;"><i class="fa fa-eye"></i></a>';
-           // //         // $output .= Form::open(['route' => [config('employee') . '.employee', $data->data[$i]->_id], 'method' => 'delete', 'style'=>'display:inline']);
-           // //         // $output .= ' <button class="btn btn-danger btn-xs" type="submit"><i class="fa fa-times"></i></button>';
-           //             // $output .= Form::close();
-           //         $data->data[$i]->action = (string)$output;
-           //     }
+               
             }
            $out->setData($data);
            return $out;
