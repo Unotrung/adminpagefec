f2b2535b43daea9d1d46e10acdf799df43713493
Anh Huy <lebuuanhhuyle@gmail.com>
2022-05-06 12:08:40 +0700

    update

--- resources/views/vendor/adminlte/bnpl/bnpl.blade.php
+++ resources/views/vendor/adminlte/bnpl/bnpl.blade.php
@@ -133,7 +133,7 @@
                     <div class="input-group date" id="reservationdate" data-target-input="nearest">
                       <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate"/>
                       <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
-                          <div class="input-group-text"><i class="fa fa-calendar" rel="tooltip" title="From Date"></i></div>
+                          <div class="input-group-text" rel="tooltip" title="From Date"><i class="fa fa-calendar" ></i></div>
                       </div>
                     </div>
                   </div>
@@ -141,7 +141,7 @@
                     <div class="input-group date" id="reservationdateto" data-target-input="nearest">
                       <input type="text" class="form-control datetimepicker-input" data-target="#reservationdateto"/>
                       <div class="input-group-append" data-target="#reservationdateto" data-toggle="datetimepicker">
-                          <div class="input-group-text"><i class="fa fa-calendar" rel="tooltip" title="To Date"></i></div>
+                          <div class="input-group-text" rel="tooltip" title="To Date"><i class="fa fa-calendar" ></i></div>
                       </div>
                     </div>
                   </div>
