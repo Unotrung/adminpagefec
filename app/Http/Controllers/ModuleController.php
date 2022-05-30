ea28c3146094f3e8ccd6ad2e88ee4d58afd3a50a
trungta-at-168084223882 <trung.ta@wolfconsulting.com.sg>
2022-05-30 14:05:37 +0700

    udpate contruct

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -12,6 +12,10 @@ use Maklad\Permission\Models\Permission;
 
 class ModuleController extends Controller
 {
+    public function __construct()
+    {
+         $this->middleware('auth');
+    }
       /**
      * Display a listing of the resource.
      *
