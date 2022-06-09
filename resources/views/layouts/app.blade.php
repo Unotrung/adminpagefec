263fc71f7273b75fb67c78a47f4b2e222a2fe68b
trungta-at-168084223882 <trung.ta@wolfconsulting.com.sg>
2022-06-09 17:32:55 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 @extends('adminlte::page')
 @section('footer')
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.1.3 07-06-2022
+      <b>Version</b> 1.1.4 09-06-2022
     </div>
     <strong>Copyright &copy; 2022 <a href="#">AdminLTE.io</a>.</strong> All rights reserved.
 @stop
